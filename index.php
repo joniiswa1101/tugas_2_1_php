17d61593936e338db85306811a155a3d5e25f0cc
Joni Iswanto <joniiswa1101@gmail.com>
2019-10-05 11:31:38 +0700

    Tugas 2-1 PHP
    Aritmatika

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+$jumlah_hari = 36;
+$jumlah_pekerja =12;
+$jumlah_pekerja2 = 24;
+
+echo "$jumlah_hari Hari = $jumlah_pekerja pekerja <br>";
+echo "X hari = $jumlah_pekerja2 <br>";
+echo "X = ...?<br>";
+$jumlah_hari2 = $jumlah_hari / ($jumlah_pekerja2 / $jumlah_pekerja) ;
+echo "Jawaban : $jumlah_hari2";
+
+ ?>
